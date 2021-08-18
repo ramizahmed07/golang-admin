95ce795e6d68e7ac94bb11f5208ea8389fbc5190
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-08-19 03:57:30 +0500

    Get body and validalidaate it

--- controllers/authController.go
+++ controllers/authController.go
@@ -7,12 +7,23 @@ import (
 
 // Register - home route controller
 func Register(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "passwords don't match",
+		})
+	}
+
 	user := models.User{
-		ID:        1,
-		FirstName: "Ramizzz",
-		LastName:  "Ahmed",
-		Email:     "ramizahmedar@gmail.com",
-		Password:  "ramiz123",
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  data["password"],
 	}
 	return c.JSON(user)
 	// return c.SendString("Hello, World 👋!")
