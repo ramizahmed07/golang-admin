d4482a7b66e56db5b4c7305c7c226014a62cc854
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-08-19 21:15:53 +0500

    Get user by cookie

--- controllers/authController.go
+++ controllers/authController.go
@@ -87,3 +87,26 @@ func Login(c *fiber.Ctx) error {
 		"message": "success",
 	})
 }
+
+// Claims - claims struct for token parsing
+type Claims struct {
+	jwt.StandardClaims
+}
+
+// User - gets user
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+	claims := token.Claims.(*Claims)
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	return c.JSON(user)
+}
