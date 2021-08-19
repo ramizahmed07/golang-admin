fa85e974be7e0c9dbeac0c1e2868b270c2d5040b
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-08-19 23:41:11 +0500

    Modify json fields

--- models/user.go
+++ models/user.go
@@ -1,9 +1,10 @@
 package models
 
+// User Model
 type User struct {
-	ID        uint
-	FirstName string
-	LastName  string
-	Email     string `gotm:"unique"`
-	Password  []byte
+	ID        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email" gorm:"unique"`
+	Password  []byte `json:"-"`
 }
