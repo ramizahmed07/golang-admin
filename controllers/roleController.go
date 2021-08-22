eda02aabfeb3fb3e5861cf06d428a0aa24bf49d6
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-08-22 16:02:44 +0500

    Add POST role

--- controllers/roleController.go
+++ controllers/roleController.go
@@ -13,3 +13,16 @@ func AllRoles(c *fiber.Ctx) error {
 	database.DB.Find(&roles)
 	return c.JSON(roles)
 }
+
+// CreateRole - creates role
+func CreateRole(c *fiber.Ctx) error {
+	var role models.Role
+
+	if err := c.BodyParser(&role); err != nil {
+		return err
+	}
+
+	database.DB.Create(&role)
+
+	return c.JSON(role)
+}
